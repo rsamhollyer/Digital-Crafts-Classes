f608e47072e8400f81076a13a62735aec3066e76
Sam Hollyer <rsamhollyer@gmail.com>
2020-10-13 10:29:58 -0500

    Added Variables and var-ex:

---
+++ Programming-101/variables-exercise.py
@@ -0,0 +1,8 @@
+greeting = "Hello everybody! My name is:"
+name = "Sam Hollyer"
+age = "37 years old! Goodbye"
+
+print(greeting)
+print(name)
+print("I am:")
+print(age)
---
+++ Programming-101/variables.py
@@ -0,0 +1,11 @@
+message = 'Hello World'
+print(message)
+
+my_name= "Sam"
+print(my_name)
+
+age = 37
+print(age)
+
+age = 39
+print(age)
