31c492c1e393c7f76480a4e3d9f18b33bc5420d6
Sam Hollyer <rsamhollyer@gmail.com>
2020-10-13 11:26:22 -0500

    Added numbers and numbers-exercise

---
+++ Programming-101/numbers-exercise.py
@@ -0,0 +1,20 @@
+print(7331 + 7331)
+
+highschool_grad= 2002
+age = 37
+print(age/highschool_grad)
+
+my_number = 567
+increment_by_number = 3
+print(my_number)
+print(increment_by_number)
+
+
+my_number += increment_by_number
+print("My new number is",my_number)
+
+my_number += increment_by_number
+print("My new number is",my_number)
+
+my_number += increment_by_number
+print("My new number is",my_number)
---
+++ Programming-101/numbers.py
@@ -0,0 +1,15 @@
+a = 20
+b = 30
+
+print(a + b)
+print(a)
+print(b)
+
+c = a + b
+
+print(c) 
+
+a = 22
+
+a += a
+print(a)
