3fcd6105563d210793d79e507933384a497c3b1d
Sam Hollyer <rsamhollyer@gmail.com>
2020-10-13 10:46:24 -0500

    Stuff added

--- Programming-101/variables-exercise.py
+++ Programming-101/variables-exercise.py
@@ -2,8 +2,4 @@ greeting = "Hello everybody! My name is:"
 name = "Sam Hollyer"
 age = 37
 
-print(greeting)
-print(name)
-print("I am:")
-print(age, "Years old")
-print("Goodbye")
+print("%s %s. I am %s years old. Goodbye!" % (greeting,name, age))
