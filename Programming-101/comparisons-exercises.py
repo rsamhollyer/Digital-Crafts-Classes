5609713ccb6e8c1d62cc7312896627a65faadaa4
Sam Hollyer <rsamhollyer@gmail.com>
2020-10-13 14:40:57 -0500

    Added comparison

---
+++ Programming-101/comparisons-exercises.py
@@ -0,0 +1,22 @@
+#1
+
+print(1 == 3, 4 <= 4, "a" == "a", 10 > 11, "b" > "c")
+
+
+#2
+my_number = 100
+number_one = 100
+number_two = 200
+number_three = 300
+
+print(my_number == number_one)
+print(my_number == number_two)
+print(my_number == number_three)
+
+#3
+name = "Sam"
+
+if name == "Sam":
+    print("YES!")
+else:
+    print("NO!")
---
+++ Programming-101/comparisons.py
@@ -0,0 +1,37 @@
+print(1 == 2) #always evaluates to True or False, nothing else
+
+
+name = "Sam"
+
+print(name != "George")
+
+print(name == "Sam")
+
+print(name > "a") #Capitalized items come 'first' and are less. It's weird but comes first is lower
+
+if 1 > 3:
+    print("This will not be printed")
+else:
+    print("this will be printed")
+    
+if False:
+    print("When will this run") #never will run because False is False and will not run a conditional
+
+if True:
+    print("When is True falsey?") #Never, this will always run
+    
+did_something = True
+
+if did_something:
+    print("check")
+    
+if None:
+    print("None is never printed")
+    
+##Falsey Statements
+# None
+# False
+# 0
+# ""
+# []
+# {}
