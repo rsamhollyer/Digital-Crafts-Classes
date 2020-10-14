25b2c447a60b1dd16af7dfc7e47495953baa2704
Sam Hollyer <rsamhollyer@gmail.com>
2020-10-14 11:40:46 -0500

    added elif

---
+++ Programming-101/elif-exercise.py
@@ -0,0 +1,9 @@
+number = int(input("Choose a random number of pizza slices!\n"))
+random_number = 50
+
+if number > random_number:
+    print("THAT'S A LOT OF PIZZA!")
+elif number == random_number:
+    print("THAT'S PERFECT") 
+else:
+    print("MOAR")   
---
+++ Programming-101/elif.py
@@ -0,0 +1,15 @@
+# age = input("How old are you? \n")
+
+# if int(age) >= 21:
+#     print("Grab a beer!")
+# else:
+#     print("GO HOME!")
+
+age = int(input("How old are you?\n"))
+
+if age == 21:
+    print("PARTAE Age!")
+elif age >21:
+    print("YOU ARE OLD")
+else:
+    print("TOO YOUNG")
