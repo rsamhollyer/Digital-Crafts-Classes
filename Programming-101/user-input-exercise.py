0d93fb8d311ddecdf91348c2f09864b08f9d1fcb
Sam Hollyer <rsamhollyer@gmail.com>
2020-10-14 11:02:03 -0500

    Added user input

---
+++ Programming-101/user-input-exercise.py
@@ -0,0 +1,10 @@
+#1
+name_one = input('Hi there. What is your name?\n')
+print(f"So happy to meet you {name_one}. Welcome to our cult!!!")
+
+
+#2
+first_name = input('Please enter your first name. \n')
+last_name = input('Please enter your last name. \n')
+
+print(f"Welcome {first_name} {last_name}. We love spaceships, Flavor-Aid, and robes. Please have a cookie!")
---
+++ Programming-101/user-input.py
@@ -0,0 +1,10 @@
+name = input('Name please:\n')
+
+subject = input('Favorite Subject \n')
+
+age = input("How old are you?")
+
+if int(age) >= 21:
+    print("Grab a beer")
+    
+print(f"You said your name is {name} and your favorite subject is {subject}")
