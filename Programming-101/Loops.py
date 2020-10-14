73a0456fe68a772a4021fe366a8f3fbf28dc899b
Sam Hollyer <rsamhollyer@gmail.com>
2020-10-14 14:34:45 -0500

    added loops

---
+++ Programming-101/Loops.py
@@ -0,0 +1,16 @@
+# i = 0
+# max_number = 120
+# increment = 2
+# while i <= max_number:
+#     print(i)
+#     i += increment #i = i + 1
+
+# j = 0
+# while j < 10:
+#     print(j) #Infinite loop
+    
+
+name = ""
+
+while name != "sam":
+    name = input("Please same your name is sam\n")
---
+++ Programming-101/loops-exercise.py
@@ -0,0 +1,24 @@
+#1
+# i = 0
+# while i < 11:
+#     print(i)
+#     i += 1
+
+#2
+# j = 10
+# while j >= 0:
+#     print(j)
+#     j -= 1
+
+#3
+user_name = ""
+pass_word = ""
+counter = 0
+
+while user_name != "sam" and pass_word != "dog":
+    user_name = input("Please enter your username \n")
+    pass_word = input("Please enter your password \n")
+    counter += 1
+    if counter == 3:
+        print("You have failed to enter correct information too many times")
+        break
