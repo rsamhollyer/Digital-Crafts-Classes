30fc7acedebf47480cd20f467813d444ba164c9c
Sam Hollyer <rsamhollyer@gmail.com>
2020-10-14 11:47:12 -0500

    update elif

--- Programming-101/elif-exercise.py
+++ Programming-101/elif-exercise.py
@@ -1,5 +1,8 @@
+from random import randrange
+
 number = int(input("Choose a random number of pizza slices!\n"))
-random_number = 50
+random_number = randrange(100)
+print(f"The perfect number of pizza slices {random_number}")
 
 if number > random_number:
     print("THAT'S A LOT OF PIZZA!")
