3c572880e6d626d32914e34f68483c10c3d83c57
Sam Hollyer <rsamhollyer@gmail.com>
2020-10-15 09:13:05 -0500

    updated exercises

--- Programming-101/smallexercises/exercise_10.py
+++ Programming-101/smallexercises/exercise_10.py
@@ -1,6 +1,9 @@
 square = int(input("How big is the square? \n"))
+icon = input("Please enter any one character \n")
+if len(icon) > 1:
+    icon = input("Please enter any one character \n")
 
 i = 0
 while i < square:
-    print("*" * square)
+    print(f"{icon}" * square)
     i+=1
