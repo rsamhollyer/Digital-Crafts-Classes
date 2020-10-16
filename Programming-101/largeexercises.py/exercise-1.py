0c7fec040cb37430cbaaad798ff4d309831dca94
Sam Hollyer <rsamhollyer@gmail.com>
2020-10-16 12:54:32 -0500

    stuff

--- Programming-101/largeexercises.py/exercise-1.py
+++ Programming-101/largeexercises.py/exercise-1.py
@@ -1,5 +1,12 @@
 # Triangle Numbers
 
-your_number = int(input("Please enter your Triangle Number"))
-triangle_number = (your_number*(your_number+1))/2
-print(triangle_number)
+# your_number = int(input("Please enter your Triangle Number"))
+# triangle_number = (your_number*(your_number+1))/2
+# print(triangle_number)
+triangle_nums = []
+
+for i in range(1,101):
+    print((i * (i + 1))/2)
+    triangle_nums.append(i)
+
+print(triangle_nums)
