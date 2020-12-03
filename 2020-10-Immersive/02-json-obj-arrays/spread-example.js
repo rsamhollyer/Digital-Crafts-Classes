c9cad9e1112de17648d6aad4bac1fd17f38e5603
Sam Hollyer <rsamhollyer@gmail.com>
2020-12-03 08:48:49 -0600

    stashing

---
+++ 2020-10-Immersive/02-json-obj-arrays/spread-example.js
@@ -0,0 +1,39 @@
+
+const person = {
+    name: 'Oakley',
+    hobby: 'napping'
+};
+
+console.table(person);
+
+const copyOfPerson = {
+    // name: person.name,
+    // hobby: person.hobby
+    ...person,
+    superpower: 'eating',
+    name: 'MILLA!!!!!',
+    name: 'Chris',
+};
+console.table(copyOfPerson);
+
+
+/*
+.post_title {
+    padding: 40px;
+    padding: 30px;
+}
+*/
+
+function mySpreadOp(obj) {
+    const newObj = {};
+    const keys = Object.keys(obj);
+
+    for (let k of keys) {
+        const val = obj[k];
+        newObj[k] = val;
+    }
+    return newObj;
+}
+//console.table(copyOfPerson);
+const anotherCopy = mySpreadOp(person);
+//console.table(anotherCopy);
