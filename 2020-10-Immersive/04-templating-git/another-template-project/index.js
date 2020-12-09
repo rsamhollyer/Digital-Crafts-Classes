0ea30ec95201bf7d3ee6a2406965c2b6543ad52d
Sam Hollyer <rsamhollyer@gmail.com>
2020-12-09 11:45:30 -0600

    adding modified template for array of objs

--- 2020-10-Immersive/04-templating-git/another-template-project/index.js
+++ 2020-10-Immersive/04-templating-git/another-template-project/index.js
@@ -9,6 +9,12 @@ const server = http.createServer(app);
 const port = 3000;
 const hostname = "localhost";
 
+const db2 = [
+	{ project: "1 thing", desc: "lalalalal" },
+	{ project: "2 thing", desc: "blablahblah" },
+	{ project: "3 thing", desc: "lululul" },
+];
+
 app.engine("html", es6Renderer);
 app.set("views", "templates");
 app.set("view engine", "html");
@@ -36,6 +42,29 @@ app.get("/projects/app/:id", (req, res) => {
 		locals: {
 			title: id,
 			details,
+		},
+	});
+});
+
+//This is to try it with an array of objects
+app.get("/test", (req, res) => {
+	const myProjects = [];
+	db2.forEach((p) => myProjects.push(p.project));
+	res.render("test", {
+		locals: {
+			myProj: myProjects,
+		},
+	});
+});
+
+app.get("/test/app/:id", (req, res) => {
+	const { id } = req.params;
+	const desc = [];
+	db2.forEach((p) => desc.push(p.desc));
+	res.render("test-des", {
+		locals: {
+			title: id,
+			desc,
 		},
 	});
 });
